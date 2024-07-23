cafdff944dedc995b5487cdac7aa555f04624c24
Isak Ellmer <isak01@gmail.com>
2024-07-23 21:28:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='nnuebit',
+        version='0.0a0',
+        scripts=['bin/nnuebit', 'bin/visbit', 'bin/quantize'],
+        packages=find_packages(),
+        author='Isak Ellmer',
+        author_email='isak01@gmail.com',
+        description='An nnue trainer for bitbit.',
+        url='https://github.com/spinojara/nnuebit',
+)
