9616619c1be9c0ab6a10e1abab5b07c99b846395
Isak Ellmer <isak01@gmail.com>
2024-08-12 21:45:24 +0200

    Drop dependency

--- nnuebit/evaluatennue.py
+++ nnuebit/evaluatennue.py
@@ -4,7 +4,34 @@ import argparse
 import subprocess
 import sys
 
-from tcadjust.tcadjust import tcadjust
+def tcadjust(tc):
+    f = open('/etc/bitbit/tcfactor', 'r')
+    tcfactor = float(f.read().strip())
+    f.close()
+
+    moves = 0
+    maintime = 0
+    increment = 0
+
+    i = tc.find('/')
+    if i != -1:
+        moves = int(tc[:i])
+        tc = tc[i + 1:]
+    i = tc.find('+')
+    if i != -1:
+        maintime = float(tc[:i])
+        increment = float(tc[i + 1:])
+    else:
+        maintime = tc
+
+    tc = ''
+    if moves > 0:
+        tc += f'{moves}/'
+    tc += f'{tcfactor * maintime}'
+    if increment > 0:
+        tc += f'+{tcfactor * increment}'
+
+    return tc
 
 def main():
     parser = argparse.ArgumentParser()
