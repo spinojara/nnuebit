447f15cda422d2eed1d8b8956c6a6651234dfb85
Isak Ellmer <isak01@gmail.com>
2025-11-04 19:49:35 +0100

    Allow setting affinity from command line

--- nnuebit/evalnnue.py
+++ nnuebit/evalnnue.py
@@ -33,14 +33,42 @@ def tcadjust(tc):
 
     return tc
 
+def numcpus(cpus):
+    if cpus == '' or cpus.startswith(',') or cpus.endswith(',') or cpus.startswith('-') or cpus.endswith('-'):
+        raise ValueError
+
+    if ',' in cpus:
+        s = cpus.split(',', 1)
+        return concurrency(s[0]) + concurrency(s[1])
+
+    if '-' in cpus:
+        if cpus.count('-') > 1:
+            raise ValueError('Only one \'-\' per \',\'')
+        s = cpus.split('-')
+        if not s[0].isdigit():
+            raise ValueError('\'%s\' is not an integer' % (s[0], ))
+        if not s[1].isdigit():
+            raise ValueError('\'%s\' is not an integer' % (s[0], ))
+        a = int(s[0])
+        b = int(s[1])
+        if b < a:
+            raise ValueError('Invalid expression: \'%d-%d\'' % (a, b))
+
+        return b + 1 - a
+
+    if not cpus.isdigit():
+        raise ValueError('\'%s\' is not an integer' % (cpus, ))
+
+    return 1
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('bitbit', type=str, help='bitbit binary')
     parser.add_argument('book', type=str, help='Opening book (.epd)')
-    parser.add_argument('concurrency', type=int, help='Concurrency')
+    parser.add_argument('cpus', type=str, help='Cpus in the format 1,2,4-6,8')
     parser.add_argument('--tc', type=str, help='Time control', default='40/5+0.05')
     parser.add_argument('nnue', type=str, nargs='+', help='NNUE files')
-    parser.add_argument('--reference', type=str, help='bitbit binary reference', default=None)
+    parser.add_argument('--reference', type=str, help='bitbit reference binary', default=None)
 
     args = parser.parse_args()
 
@@ -49,15 +77,15 @@ def main():
         engines += 1
 
     if engines < 2:
-        print('Input at least two NNUE networks.')
+        print('Input at least two NNUE networks or one NNUE network and a reference.')
         sys.exit(1)
 
     command = ['fastchess',
-               '-use-affinity', '2,4,6,8,10',
+               '-use-affinity', args.cpus,
                '-tournament', 'roundrobin',
                '-games', '2',
                '-rounds', '10000',
-               '-concurrency', str(args.concurrency),
+               '-concurrency', str(numcpus(args.cpus)),
                '-each', 'proto=uci', f'tc={tcadjust(args.tc)}',
                '-openings', 'format=epd', f'file={args.book}', 'order=random',
                '-repeat',
